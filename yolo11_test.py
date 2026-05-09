1015106b5f1132f096b2e12e0aacd875f24ef805
inhatcmin <jhmin@inhatc.ac.kr>
2026-05-09 13:21:32 +0900

    test

--- yolo11_test.py
+++ yolo11_test.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolo11n.pt")  # pretrained YOLO26n model
+model = YOLO("runs/detect/train/weights/best.pt")  # pretrained YOLO26n model
 
 # Run batched inference on a list of images
-results = model(["image1.jpg", "image2.jpg"])  # return a list of Results objects
+results = model(["test1.jpg","test2.jpg"])  # return a list of Results objects
 
 #results =model(
 #    source="test_images/",   # 이미지 폴더
