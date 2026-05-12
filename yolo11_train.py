cfb523b0c540355f169caa340d30aa4cdd607442
inhatcmin <jhmin@inhatc.ac.kr>
2026-05-12 14:03:50 +0900

    coco128

--- yolo11_train.py
+++ yolo11_train.py
@@ -7,5 +7,5 @@ model = YOLO("yolo26n.pt")  # load a pretrained model (recommended for training)
 
 
 # Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+results = model.train(data="coco128.yaml", epochs=100, imgsz=640)
 # train parameters https://docs.ultralytics.com/ko/modes/train/#musgd-optimizer
