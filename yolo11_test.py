dbee136bc9b8192e3e36207243d516f10045feff
inhatcmin <jhmin@inhatc.ac.kr>
2026-05-17 15:34:32 +0900

    save file error fix

--- yolo11_test.py
+++ yolo11_test.py
@@ -15,11 +15,13 @@ results = model(["test1.jpg","test2.jpg"])  # return a list of Results objects
 
 
 # Process results list
-for result in results:
-    boxes = result.boxes  # Boxes object for bounding box outputs
-    masks = result.masks  # Masks object for segmentation masks outputs
-    keypoints = result.keypoints  # Keypoints object for pose outputs
-    probs = result.probs  # Probs object for classification outputs
-    obb = result.obb  # Oriented boxes object for OBB outputs
-#    result.show()  # display to screen
-    result.save(filename="result.jpg")  # save to disk
+for i, result in enumerate(results):
+
+    boxes = result.boxes
+    masks = result.masks
+    keypoints = result.keypoints
+    probs = result.probs
+    obb = result.obb
+    filename = f"result_{i}.jpg"
+    result.save(filename=filename)
+    print(f"Saved: {filename}")
